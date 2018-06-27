21e3f4fd100584de06144794ea1fa7a20ca2e23d
pwujczyk <pwujczyk@ecovadis.com>
2018-06-27 15:12:42 +0200

    initial commit

---
+++ DateTimePT/DateTimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimePT
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime SubtrackMinutes(this DateTime dt, int minutes)
+        {
+            return dt.AddMinutes(-1 * minutes);
+        }
+
+    }
+}
