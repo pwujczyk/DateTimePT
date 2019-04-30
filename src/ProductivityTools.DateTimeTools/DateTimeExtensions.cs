b484ffea1849c97f318b6f5c295751bb5cc3fd62
pwujczyk <pwujczyk@ecovadis.com>
2019-04-30 16:30:33 +0200

    add net core support

---
+++ src/ProductivityTools.DateTimeTools/DateTimeExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductivityTools.DateTimeTools
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime SubtrackMinutes(this System.DateTime dt, int minutes)
+        {
+            return dt.AddMinutes(-1 * minutes);
+        }
+
+        public static DateTime SubtrackDays(this DateTime dt, int days)
+        {
+            return dt.AddDays(-1 * days);
+        }
+
+    }
+}
---
+++ src/ProductivityTools.DateTimeTools/DateTimePT.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductivityTools.DateTimeTools
+{
+    public class DateTimePT : IDateTimePT
+    {
+        public System.DateTime Now => System.DateTime.Now;
+    }
+}
